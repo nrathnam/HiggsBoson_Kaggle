c23844894ca4e8532e7ba38091047b1c5ed60af5
Bernard Ong <bernard.ong@entense.com>
2016-08-18 14:08:15 -0400

    Test push only.

--- source_baseline/higgs_nn.py
+++ source_baseline/higgs_nn.py
@@ -26,34 +26,34 @@ def process_training_data(filename, features, impute, standardize, whiten):
     Reads in training data and prepares numpy arrays.
     """
     training_data = pd.read_csv(filename, sep=',')
-    
+
     # add a nominal label (0, 1)
     temp = training_data['Label'].replace(to_replace=['s', 'b'], value=[1, 0])
     training_data['Nominal'] = temp
-    
+
     X = training_data.iloc[:, 1:features+1].values
     y = training_data.iloc[:, features+3].values
     w = training_data.iloc[:, features+1].values
-    
+
     # optionally impute the -999 values
     if impute == 'mean':
         imp = preprocessing.Imputer(missing_values=-999)
         X = imp.fit_transform(X)
     elif impute == 'zeros':
         X[X == -999] = 0
-    
+
     # create a standardization transform
     scaler = None
     if standardize:
         scaler = preprocessing.StandardScaler()
         scaler.fit(X)
-    
+
     # create a PCA transform
     pca = None
     if whiten:
         pca = decomposition.PCA(whiten=True)
         pca.fit(X)
-    
+
     return training_data, X, y, w, scaler, pca
 
 
@@ -63,21 +63,21 @@ def create_nn_pre_train_file(original_filename, new_filename, impute, scaler, pc
     by pylearn2's csv data set class.
     """
     combined_data = pd.read_csv(original_filename, sep=',')
-    
+
     X = combined_data.values
-    
+
     if impute == 'mean':
         imp = preprocessing.Imputer(missing_values=-999)
         X = imp.fit_transform(X)
     elif impute == 'zeros':
         X[X == -999] = 0
-    
+
     if scaler is not None:
         X = scaler.transform(X)
-    
+
     if pca is not None:
         X = pca.transform(X)
-    
+
     combined_data = pd.DataFrame(X, columns=combined_data.columns.values)
     combined_data.to_csv(new_filename, sep=',', index=False)
 
@@ -88,30 +88,30 @@ def create_nn_training_file(training_data, features, impute, scaler, pca, filena
     by pylearn2's csv data set class.
     """
     nn_training_data = training_data
-    
+
     nn_training_data.insert(0, 'NN_Label', nn_training_data['Nominal'].values)
-    
+
     nn_training_data.drop('EventId', axis=1, inplace=True)
     nn_training_data.drop('Weight', axis=1, inplace=True)
     nn_training_data.drop('Label', axis=1, inplace=True)
     nn_training_data.drop('Nominal', axis=1, inplace=True)
-    
+
     X = nn_training_data.iloc[:, 1:features+1].values
-    
+
     if impute == 'mean':
         imp = preprocessing.Imputer(missing_values=-999)
         X = imp.fit_transform(X)
     elif impute == 'zeros':
         X[X == -999] = 0
-    
+
     if scaler is not None:
         X = scaler.transform(X)
-    
+
     if pca is not None:
         X = pca.transform(X)
-    
+
     X = np.insert(X, 0, nn_training_data['NN_Label'].values, 1)
-    
+
     nn_training_data = pd.DataFrame(X, columns=nn_training_data.columns.values)
     nn_training_data.to_csv(filename, sep=',', index=False)
 
@@ -122,7 +122,7 @@ def train(model_definition_file, data_dir):
     """
     with open(model_definition_file, 'r') as f:
         train_nn = f.read()
-    
+
     hyper_params = {'data_dir': data_dir,
                     'num_features': 30,
                     'dim_h0': 50,
@@ -147,23 +147,23 @@ def predict(X, threshold, scaler, pca, model_file):
     """
     if scaler is not None:
         X = scaler.transform(X)
-    
+
     if pca is not None:
         X = pca.transform(X)
-    
+
     # Load the model
     model = serial.load(model_file)
-    
+
     # Create Theano function to compute probability
     x = model.get_input_space().make_theano_batch()
     y = model.fprop(x)
     pred = function([x], y)
-    
+
     # Convert to a prediction
     y_prob = pred(X)[:, 1]
     cutoff = np.percentile(y_prob, threshold)
     y_est = y_prob > cutoff
-    
+
     return y_prob, y_est
 
 
@@ -175,9 +175,9 @@ def score(y, y_est, w):
     y_background = w * (y == 0.0)
     s = np.sum(y_signal * (y_est == 1.0))
     b = np.sum(y_background * (y_est == 1.0))
-    
+
     return ams(s, b)
-    
+
 
 def process_test_data(filename, features, impute):
     """
@@ -185,13 +185,13 @@ def process_test_data(filename, features, impute):
     """
     test_data = pd.read_csv(filename, sep=',')
     X_test = test_data.iloc[:, 1:features+1].values
-    
+
     if impute == 'mean':
         imp = preprocessing.Imputer(missing_values=-999)
         X_test = imp.fit_transform(X_test)
     elif impute == 'zeros':
         X_test[X_test == -999] = 0
-    
+
     return test_data, X_test
 
 
@@ -205,20 +205,20 @@ def create_submission(test_data, y_test_prob, y_test_est, submit_file):
 
     # sort it so they're in the ascending order by probability
     submit = submit.sort(['RankOrder'], ascending=True)
-    
+
     # convert the probabilities to rank order (required by the submission guidelines)
     for i in range(0, y_test_est.shape[0], 1):
         submit.iloc[i, 1] = i + 1
-    
+
     # re-sort by event ID
     submit = submit.sort(['EventId'], ascending=True)
-    
+
     # convert the integer classification to (s, b)
     submit['Class'] = submit['Class'].map({1: 's', 0: 'b'})
-    
+
     # force pandas to treat these columns at int (otherwise will write as floats)
     submit[['EventId', 'RankOrder']] = submit[['EventId', 'RankOrder']].astype(int)
-    
+
     # finally create the submission file
     submit.to_csv(submit_file, sep=',', index=False, index_label=False)
 
@@ -245,49 +245,49 @@ def main():
     training_nn_file = 'training_nn.csv'
     model_definition_file = 'mlp.yaml'
     model_file = 'mlp.pkl'
-    
+
     os.chdir(code_dir)
-    
+
     print 'Starting process...'
     print 'impute={0}, standardize={1}, whiten={2} threshold={3}'.format(
         impute, standardize, whiten, threshold)
-    
+
     if load_training_data:
         print 'Reading in training data...'
         training_data, X, y, w, scaler, pca = process_training_data(
             data_dir + training_file, features, impute, standardize, whiten)
-    
+
     if train_model:
         print 'Running neural network process...'
-        
+
         if create_nn_files:
             print 'Creating training files...'
             create_nn_training_file(training_data, features, impute, scaler, pca,
                 data_dir + training_nn_file)
             create_nn_pre_train_file(data_dir + pretrain_file,
                 data_dir + pretrain_nn_file, impute, scaler, pca)
-        
+
         if train_nn_model:
             print 'Training the model...'
             train(code_dir + model_definition_file, data_dir)
-        
+
         print 'Calculating predictions...'
         y_prob, y_est = predict(X, threshold, scaler, pca, data_dir + model_file)
-        
+
         print 'Calculating AMS...'
         ams_val = score(y, y_est, w)
         print 'AMS =', ams_val
-    
+
     if create_submission_file:
         print 'Reading in test data...'
         test_data, X_test = process_test_data(data_dir + test_file, features, impute)
-        
+
         print 'Predicting test data...'
         y_test_prob, y_test_est = predict(X_test, threshold, scaler, pca, data_dir + model_file)
-        
+
         print 'Creating submission file...'
         create_submission(test_data, y_test_prob, y_test_est, data_dir + submit_file)
-    
+
     print 'Process complete.'
 
 
