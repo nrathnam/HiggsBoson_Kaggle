0d19475f081ee6f1e5019971d5994c710d6f6923
dkhurana1306 <deepak.khurana@nyu.edu>
2016-08-20 11:37:05 -0400

    Response EDA

--- Data_Processing/data_processing.R
+++ Data_Processing/data_processing.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(VIM)
 library(mice)
 library(PASWR)
+library(ggplot2)
+library(MASS)
 
 data = read.csv('/Users/dk1306/downloads/training.csv', header=T)
 
@@ -44,11 +46,49 @@ aggr(data)                   # Display Missinggness
 md.pattern(data)             # Missingness in numerical format 
 
 #####################################################################################
-scaled_Weight = Weight/max(Weight)
-n.bin = 200
-labels=c(1:n.bin )/n.bin
-f = cut(scaled_Weight,n.bin ,labels=labels)
-table(Label,f)
+
+Response = as.data.frame(cbind("Label" = as.factor(Label),Weight))       # EDA on predictions  
+Response$Label = Label
+
+#~~~~~~~~~~~~~
+
+n.bin = 20                                         # Number of categories to split Weight
+labels=c(1:n.bin)                                  # Labeling the new categories 
+f = cut(Response$Weight,n.bin ,labels=labels)      # Categorical variable for scaled weights
+tbl = table(Label,f)
+tbl
+#Label     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15    16    17    18    19    20
+#b 23389 10550  5629 11176 23949 24594 12135  5834  1343   358  3278 11736 12959  6762  4235  2780  2023  1138   413    52
+#s 85667     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0
+
+chisq.test(tbl)                       # Weights and Label are dependent on each other 
+#Pearson's Chi-squared test
+#data:  tbl
+#X-squared = 168430, df = 19, p-value < 2.2e-16
+
+#~~~~~~~~~~~~~
+
+max((Response$Weight[Response$Label == 's'])) < min(Response$Weight[Response$Label == 'b']) # Signal and background are mutually exclusive in weights
+#[1] TRUE
+
+#Signal events have low weights 
 
 
+#~~~~~~~~~~~~~
 
+
+g = ggplot(data = Response, aes(x =Weight))
+g + geom_histogram(aes(fill = Label), binwidth = 0.1)
+ggsave("/Users/dk1306/nycdsa-kaggle-project/Data_Processing/s_AND_b_weight_histogram.png")
+
+
+g = ggplot(data = Response[Response$Weight <= max(Response$Weight[Response$Label == 's']),], aes(x =Weight))
+g + geom_histogram(aes(fill = Label), binwidth = 0.0002)
+ggsave("/Users/dk1306/nycdsa-kaggle-project/Data_Processing/s_weight_histogram.png")
+
+g = ggplot(data = Response[Response$Weight > max(Response$Weight[Response$Label == 's']),], aes(x =Weight))
+g + geom_histogram(aes(fill = Label), binwidth = 0.1)
+ggsave("/Users/dk1306/nycdsa-kaggle-project/Data_Processing/b_weight_histogram.png")
+
+
+#####################################################################################
