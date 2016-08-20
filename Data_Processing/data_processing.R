2674b26af562f94376c98a142aa98fde5dcdffb6
dkhurana1306 <deepak.khurana@nyu.edu>
2016-08-20 08:31:29 -0400

    NA - 999 fixed

--- Data_Processing/data_processing.R
+++ Data_Processing/data_processing.R
@@ -11,16 +11,16 @@ Weight  = data$Weight
 Label   = data$Label
 
 data = data[, -c(1,32,33)]          # Get rid of EventId Weight Label
-PRI_jet_num = data$PRI_jet_num      # Jet number is a category variable so donot scale
-data = as.data.frame(scale(data))   # scale data , it scale jet number also 
-data$PRI_jet_num = PRI_jet_num      # Unscale jet number back to its integer value
+PRI_jet_num = data$PRI_jet_num      # Jet number is a category variable so do not scale it
+data = as.data.frame(scale(data))   # Scale data, it is scaling Jet number also 
+data$PRI_jet_num = PRI_jet_num      # Use unscaled Jet number in the data
 
 tf = colSums(is.na(data)) !=0       # Columns missing any data 
 
-length(colnames(data)[tf])          # Number of columns 
+length(colnames(data)[tf])          # Number of columns missing any data
 #[1] 11
 
-colSums(is.na(data))[colnames(data)[colSums(is.na(data)) !=0] ]/nrow(data)*100     # Column names and % of missing data 
+colSums(is.na(data))[colnames(data)[colSums(is.na(data)) !=0] ]/nrow(data)*100     # Column Names and % of missing data 
 #DER_mass_MMC   DER_deltaeta_jet_jet       DER_mass_jet_jet    DER_prodeta_jet_jet DER_lep_eta_centrality 
 #15.2456                70.9828                70.9828                70.9828                70.9828 
 #PRI_jet_leading_pt    PRI_jet_leading_eta    PRI_jet_leading_phi  PRI_jet_subleading_pt PRI_jet_subleading_eta 
@@ -28,15 +28,15 @@ colSums(is.na(data))[colnames(data)[colSums(is.na(data)) !=0] ]/nrow(data)*100  
 #PRI_jet_subleading_phi 
 #70.9828
 
-colnames(data)[which(colSums(is.na(data))[colnames(data)[colSums(is.na(data)) !=0] ]/nrow(data)*100 >70)] # Columns missing more than 70 % data
+colnames(data)[which(colSums(is.na(data))[colnames(data)[colSums(is.na(data)) !=0] ]/nrow(data)*100 >70)] # Name of Columns missing more than 70 % data
 #[1] "DER_mass_transverse_met_lep" "DER_mass_vis"                "DER_pt_h"                    "DER_deltaeta_jet_jet"       
 #[5] "DER_pt_tot"                  "DER_sum_pt"                  "DER_pt_ratio_lep_tau" 
 
-sum(is.na(data))/ (nrow(data)* ncol(data))*100    # % of cells missing datasum(complete.cases(data)) / nrow(data)*100
+sum(is.na(data))/ (nrow(data)* ncol(data))*100    # % of cells missing data
 #[1] 21.06736
 
 sum(complete.cases(data)) / nrow(data)*100    # % of events/rows with complete data
 #[1] 27.2456
 
-aggr(data)
-md.pattern(data)
+aggr(data)                   # Display Missinggness
+md.pattern(data)             # Missingness in numerical format 
